19cc264aae9a0e6a3acee3882ed800710946d0f0
Quang2110 <haminhquang09869@gmail.com>
2024-07-28 18:40:26 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: ' Hee loo bạn nhỏ !!!',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'Oki lun nhớ',
+    title: 'Em có thấy anh đẹp trai cool lại còn yêu em nhất hệ mặt trời không  ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em đồng ý đó nha :> ',
+    btnYes: 'Em đồng ý 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao em đồng ý được khum 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai đó Ahiiiii 😊😊',
+    mess: 'Cám ơn bé vì đã cho anh cơ hội hẹn hò cùng bé nha anh yêu bé HinHin nhiều lắm 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Okê la la 💖',
+    messLink: '//www.facebook.com/profile.php?id=100006181004637'
+}
